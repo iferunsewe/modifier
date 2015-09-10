09db0aa62664eabca31c4c6e27cce0c6be9a14e2
Ife Runsewe <ife.runsewe@magentys.io>
2015-09-10 11:28:43 +0100

    Rearranged methods in combiner

--- refactoring/lib/combiner.rb
+++ refactoring/lib/combiner.rb
@@ -17,41 +17,57 @@ class Combiner
 	def combine(*enumerators)
 		Enumerator.new do |yielder|
 			last_values = Array.new(enumerators.size)
-			done = enumerators.all? { |enumerator| enumerator.nil? }
-			while not done
-				last_values.each_with_index do |value, index|
-					if value.nil? and not enumerators[index].nil?
-						begin
-							last_values[index] = enumerators[index].next
-						rescue StopIteration
-							enumerators[index] = nil
-						end
-					end
-				end
+      done = enumerators.all? { |enumerator| enumerator.nil? }
+			while !done
+        last_values = select_values_from_enum(last_values, enumerators)
 
 				done = enumerators.all? { |enumerator| enumerator.nil? } and last_values.compact.empty?
 				unless done
-					min_key = last_values.map { |e| key(e) }.min do |a, b|
-						if a.nil? and b.nil?
-							0
-						elsif a.nil?
-							1
-						elsif b.nil?
-							-1
-						else
-							a <=> b
-						end
-					end
-					values = Array.new(last_values.size)
-					last_values.each_with_index do |value, index|
-						if key(value) == min_key
-							values[index] = value
-							last_values[index] = nil
-						end
-					end
-					yielder.yield(values)
+					yielder.yield(select_values_from_min_key(last_values))
 				end
 			end
 		end
-	end
+  end
+
+  private
+
+    def select_values_from_enum(last_values, enumerators)
+      last_values.map.with_index do |value, index|
+        if value.nil? && !enumerators[index].nil?
+          begin
+            enumerators[index].next
+          rescue StopIteration
+            enumerators[index] = nil
+          end
+        else
+          value
+        end
+      end
+    end
+
+    def select_values_from_min_key(last_values)
+      min_key = min_key(last_values)
+      values = Array.new(last_values.size)
+      last_values.each_with_index do |value, index|
+        if key(value) == min_key
+          values[index] = value
+          last_values[index] = nil
+        end
+      end
+      values
+    end
+
+    def min_key(last_values)
+      last_values.map { |e| key(e) }.min do |a, b|
+        if a.nil? and b.nil?
+          0
+        elsif a.nil?
+          1
+        elsif b.nil?
+          -1
+        else
+          a <=> b
+        end
+      end
+    end
 end
